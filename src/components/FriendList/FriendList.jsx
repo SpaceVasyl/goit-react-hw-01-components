d62269bdc83a4bdc754d9c4845695af499629f10
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-03-20 01:41:52 +0200

    1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,13 @@
-
+import css from './FriendList.module.css'
 export const FriendList = ({friends}) => (
-    <ul className="friend-list">
-    {friends.map(friend=> (<li key={friend.id} className="item"><span className="status">{friend.isOnline}</span>
-    <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
-    <p className="name">{friend.name}</p>
+    <ul className={css.friendList}>
+    {friends.map(friend=> (<li key={friend.id} className={css.item}><div className={css.status}>
+        {friend.isOnline === true
+          ? <div className={css.trues}></div>
+          : <div className={css.falces}></div>}
+    </div>
+    <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
+    <p className={css.name}>{friend.name}</p>
     </li>))}
     </ul>
 )
