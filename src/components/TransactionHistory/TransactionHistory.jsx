e8762f6c6ee2da1153e44da7dded660e4eceb4b1
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-03-20 02:12:18 +0200

    1

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,6 +1,19 @@
+import css from './TransactionHistory.module.css'
+import PropTypes from "prop-types";
 export const TransactionHistory = ({transactions}) => (
-<ul>
-<li><span> TYPE </span><span> AMOUNT </span><span> CURRENSY </span></li>
-{transactions.map(transaction =>(<li key={transaction.id}><span>{transaction.type}</span><span>{transaction.amount}</span><span>{transaction.currency}</span></li>))}
+<ul className={css.table}>
+<li className={css.firstRowElement}><span>TYPE</span><span>AMOUNT</span><span>CURRENSY</span></li>
+{transactions.map(transaction =>(<li key={transaction.id} className={css.element}><span>{transaction.type}</span><span>{transaction.amount}</span><span>{transaction.currency}</span></li>))}
 </ul>
 )
+
+TransactionHistory.propTypes = {
+    transactions: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+  };
